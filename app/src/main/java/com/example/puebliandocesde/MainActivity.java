28f030ae4df305099b3aeb9348f58aafe4d34011
dicojo <diniercj@gmail.com>
2023-06-21 19:54:26 -0500

    Se hacen muchas cosas: Se empieza a programar en java con clases para reproducir audio, se crea xml para modelar botón

--- app/src/main/java/com/example/puebliandocesde/MainActivity.java
+++ app/src/main/java/com/example/puebliandocesde/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.puebliandocesde;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
+   //Atributos = elementos de la actividad
+    MediaPlayer Tonito;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Tonito = MediaPlayer.create(this, R.raw.audio_pavido);
+        Tonito.start();
+
+        //Se temporize el Splash
+        TimerTask inicioAPP = new TimerTask() {
+            @Override
+            public void run() {
+                //Activo/lanzo el Home
+                Intent lanjamiento = new Intent(MainActivity.this,Home.class);
+                startActivity(lanjamiento);
+            }
+        };
+    Timer lapso = new Timer();
+    lapso.schedule(inicioAPP, 10000);
     }
 }
